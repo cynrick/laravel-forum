ea45a7c778ef702fbb662f891693de04c7dc0bee
Cynrick Chiu <cyn.2k5@outlook.com>
2017-08-15 14:39:50 +0900

    Define factory for populating database with Reply data

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -32,3 +32,15 @@ $factory->define(App\Thread::class, function ($faker) {
 		'body' => $faker->paragraph
 	];
 });
+
+$factory->define(App\Reply::class, function ($faker) {
+	return [
+		'user_id' => function () {
+			return factory('App\User')->create()->id;
+		},
+		'thread_id' => function () {
+			return factory('App\Thread')->create()->id;
+		},
+		'body' => $faker->paragraph
+	];
+});
